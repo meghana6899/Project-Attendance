4fe99ce83d9320dc1b8e1601ab2c7feaa0b7d453
meghana6899 <meghana.bamr@gmail.com>
2025-04-17 12:40:33 +0530

    Changes

--- frontend/src/api/queries/workHours.jsx
+++ frontend/src/api/queries/workHours.jsx
@@ -1,10 +1,17 @@
+
+
 import React from 'react'
 import axios from 'axios'
 
 async function workHours(date) {
-    const id = localStorage.getItem('id')
-    const role = localStorage.getItem('role');
-    const user = role === "admin" || "employee" ? "employee" : "student"
+    const user = JSON.parse(localStorage.getItem('user'));
+    var users;
+    if (user.role === 'admin' || user.role === 'employee') {
+        users = 'employee'
+    }
+    else {
+        users = 'student'
+    }
     const response = await axios.post(`http://localhost:3000/api/hours/${user}/${id}`, {
         date
     })
